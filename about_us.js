787f60f3f88ef699c6356cea1e675f1bc9d25424
paulmalagoli <73070843+paulmalagoli@users.noreply.github.com>
2020-12-13 01:02:02 +0100

    NOUVELLE VERSION

---
+++ about_us.js
@@ -0,0 +1,90 @@
+function myFunction1() {
+  var description = document.getElementById("description1");
+  var myBtn = document.getElementById("myBtn1");
+  
+    
+  if (description.style.display == "none") {	
+	description.style.display = "inline";
+	myBtn.innerHTML = "Voir moins";
+  } 
+  else {
+	description.style.display = "none";
+	myBtn.innerHTML = "Voir plus";	
+  }
+}
+
+function myFunction2() {
+  var description = document.getElementById("description2");
+  var myBtn = document.getElementById("myBtn2");
+  
+  
+  if (description.style.display == "none") {
+    description.style.display = "inline";
+	myBtn.innerHTML = "Voir moins";    
+  } 
+  else {
+    description.style.display = "none";    
+	myBtn.innerHTML = "Voir plus";
+  }
+}
+
+function myFunction3() {
+  var description = document.getElementById("description3");
+  var myBtn = document.getElementById("myBtn3");
+  
+  
+  if (description.style.display == "none") {
+    description.style.display = "inline";
+	myBtn.innerHTML = "Voir moins";    
+  } 
+  else {
+    description.style.display = "none";    
+	myBtn.innerHTML = "Voir plus";
+  }
+}
+
+function myFunction4() {
+  var description = document.getElementById("description4");
+  var myBtn = document.getElementById("myBtn4");
+  
+  
+  if (description.style.display == "none") {
+    description.style.display = "inline";
+	myBtn.innerHTML = "Voir moins";    
+  } 
+  else {
+    description.style.display = "none";    
+	myBtn.innerHTML = "Voir plus";
+  }
+}
+
+function myFunction5() {
+  var description = document.getElementById("description5");
+  var myBtn = document.getElementById("myBtn5");
+  
+  
+  if (description.style.display == "none") {
+    description.style.display = "inline";
+	myBtn.innerHTML = "Voir moins";    
+  } 
+  else {
+    description.style.display = "none";    
+	myBtn.innerHTML = "Voir plus";
+  }
+}
+
+function myFunction6() {
+  var description = document.getElementById("description6");
+  var myBtn = document.getElementById("myBtn6");
+  
+  
+  if (description.style.display == "none") {
+    description.style.display = "inline";
+	myBtn.innerHTML = "Voir moins";    
+  } 
+  else {
+    description.style.display = "none";    
+	myBtn.innerHTML = "Voir plus";
+  }
+}
+
