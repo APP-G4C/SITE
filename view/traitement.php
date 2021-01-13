be02fa7ffc24e983793b8d80af50c6eb837212ce
lafnag <73112654+lafnag@users.noreply.github.com>
2021-01-13 18:47:40 +0100

    Update traitement.php

--- view/traitement.php
+++ view/traitement.php
@@ -18,68 +18,49 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/SITE/view/page_contact.php');
   $login ="root";
   $pass="root";
 
-  $nom=$prenom=$email=$telephone=$message="";
+
+
+
+
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
+
     try{
-      $connexion = new PDO("mysql:host=$serveur;dbname=database_app", $login, $pass);
+      $connexion = new PDO("mysql:host=$serveur;dbname=database_app", $login, $pass); //connection a la bdd
       $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
       $requete = $connexion->prepare(
         "INSERT INTO contact(nom,prenom,email,telephone,message)
                     VALUES(:nom,:prenom,:email,:telephone,:message)"
         );
 
-        $requete->bindParam(':nom',$nom);
+
+        $requete->bindParam(':nom',$nom,  PDO::PARAM_STR);
         $requete->bindParam(':prenom',$prenom);
         $requete->bindParam(':email',$email);
         $requete->bindParam(':telephone',$telephone);
         $requete->bindParam(':message',$message);
 
-        $nom=test_input($_POST['lname']);
-        $prenom = $_POST['fname'];
+
+        $nom=$_POST['lname'];
+        $prenom =$_POST['fname'];
         $email= $_POST['email'];
-        $telephone= $_POST['tel'];
+        $telephone=$_POST['tel'];
         $message= $_POST['message'];
 
 
-        function test_input($data) {
-      $data = htmlspecialchars($data);
-      return $data;
-    }
       $requete->execute();
-      }
-      catch(PDOException $e){
-        echo 'echec de la co : ' .$e->getMessage();
-      }
+
+
+    }
+    catch(PDOException $e){
+      echo 'echec de la co : ' .$e->getMessage();
     }
 
-  /*  function test_input($donnee){
-      $donnee=htmlspecialchars($donnee);
-      $donnee=strip_tags($donnee);
-      $donnee=trim($donnee);
-      $donne=stripslashes($donnee);
+  }
 
-      return $donnee;
-    }*/
-
-  /*  $name = $_POST['fname'];
-    if (empty($name)) {
-      echo "Name is empty";
-    } else {
-      echo $name;
-    }
-  }*/
-
-
-
-
-
-/*Welcome  <?php echo htmlspecialchars($_POST["fname"]); ?>  <?php echo htmlspecialchars($_POST["lname"]);?> , votre adresse mail est <?php echo $_POST["email"]; ?>  <br>
-Votre question est : <br>
-<?php echo $_POST["message"]; */
 ?>
-</form>
+
 
 </body>
 </html>
