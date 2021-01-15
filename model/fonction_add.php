7158b8a179310f74694fb84d60358e2666657b3a
paulmalagoli <73070843+paulmalagoli@users.noreply.github.com>
2021-01-15 18:21:29 +0100

    Paul 15/1/21 FAQ 18h
    Paul 15/1/21 FAQ 18h

--- model/fonction_add.php
+++ model/fonction_add.php
@@ -34,4 +34,21 @@ function fonction_add_utilisateur(){
             $test=true;
             $stmt->execute();
 }
-            ?>
+
+function fonction_add_faq(){
+    $pdo=new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+        $sql=" INSERT INTO `FAQ`(`Question`, `Reponse`) VALUES (:Question, :Reponse)";
+        $stmt = $pdo->prepare($sql);
+            // On attache les variables au statement comme paramètres
+            $stmt->bindParam(":Question", $param_Question, PDO::PARAM_STR);
+            $stmt->bindParam(":Reponse", $param_Reponse, PDO::PARAM_STR);
+      
+
+            // On remplis les paramètres
+            $param_Question = trim($_POST["Question"]);
+            $param_Reponse = trim($_POST["Reponse"]);
+            $test=true;
+            $stmt->execute();
+}
+
+
