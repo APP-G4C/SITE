6f608da54141921003f3e53f45a5c091ce4de6b1
TimLamare <timothe.lamare@gmail.com>
2021-01-21 17:59:46 +0100

    test phpmailer

--- new_user_mail.php
+++ new_user_mail.php
@@ -1,9 +1,38 @@
 <?php
-ini_set("SMTP", "smtp.sfr.fr");
-ini_set("sendmail_from","tmtlamare@gmail.com");
-if (mail("tmtlamare@gmail.com","test","test")){
-echo "test réussi";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+$mail->SMTPDebug  = 1;
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "G4CAPP@gmail.com";
+$mail->Password   = "G4CAPPcreatif";
+
+$mail->IsHTML(true);
+$mail->AddAddress("tmtlamare@gmail.com", "TIM");
+$mail->SetFrom("G4CAPP@gmail.com", "G4CAPP");
+$mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
+$mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+
+$mail->MsgHTML($content);
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
 }
-else {
-  echo "test raté";
-}?>
+
+?>
