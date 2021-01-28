32452ffe839185defdac874f27ba9a026fc63502
TimLamare <timothe.lamare@gmail.com>
2021-01-28 17:50:14 +0100

    Update ControllerAdmin.php

--- controller/ControllerAdmin.php
+++ controller/ControllerAdmin.php
@@ -3,7 +3,18 @@
    require_once($_SERVER['DOCUMENT_ROOT'].'/SITE/controller/ControllerSession.php');
    require_once($_SERVER['DOCUMENT_ROOT'].'/SITE/controller/ControllerConfig.php');
 
-
+$type=type_u();
+switch ($type) {
+  case 1:
+    header('Location:ControllerUser.php')
+    break;
+  case 2:
+    header('Location:ControllerGestionnaire.php')
+    break;
+  case 4:
+    header('Location:ControllerLogin.php')
+    break;
+}
 if($_SESSION['type']==3){
   $modifheader=  header_ada();
 
